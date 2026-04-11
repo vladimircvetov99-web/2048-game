6bdc8f908e5f72b9da04c288db0de80331aa654c
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-11 14:51:51 +0600

    finish the game 2048

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
 
 const gameGrid = document.querySelector('.game-grid');
 const SIZE = 4;
@@ -8,11 +8,11 @@ let board = [];
 let currentScore = 0;
 
 const currentScoreElem = document.querySelector('.current-score');
-const hightScoreElem = document.querySelector('.high-score');
+const highScoreElem = document.querySelector('.high-score');
 
-let hightScore = localStorage.getItem('2048-highScore') || 0;
+let highScore = Number(localStorage.getItem('2048-highScore')) || 0;
 
-hightScoreElem.textContent = hightScore;
+highScoreElem.textContent = highScore;
 
 const gameOverElem = document.querySelector('.game-over');
 
@@ -21,16 +21,16 @@ function updateScore(value) {
     currentScore += value;
     currentScoreElem.textContent = currentScore;
 
-    if (currentScore > hightScore) {
-        hightScore = currentScore;
-        hightScoreElem.textContent = hightScore;
-        localStorage.setInterval('2048-highScore', high-Score);
+    if (currentScore > highScore) {
+        highScore = currentScore;
+        highScoreElem.textContent = highScore;
+        localStorage.setItem('2048-highScore', highScore);
     }
 }
 
 function restartGame(){
     currentScore = 0;
-    currentScore.textContent = '0';
+    currentScoreElem.textContent = '0';
     gameOverElem.style.display = 'none';
     initializeGame();
 }
@@ -41,11 +41,11 @@ function initializeGame() {
     placeRandom();
     placeRandom();
 
-    randerBoard();
+    renderBoard();
 
 }
 
-function randerBoard() {
+function renderBoard() {
     for (let i = 0; i < SIZE; i++) {
       for (let j = 0; j < SIZE; j++) {
         const cell = document.querySelector(`[data-row="${i}"][data-col="${j}"]`);
@@ -72,17 +72,142 @@ function randerBoard() {
 
    setTimeout(() => {
     const cells = document.querySelectorAll('.grid-cell');
-    cell.forEach(cell =>{
+    cells.forEach(cell => {
         cell.classList.remove('merged-tile', 'new-tile');
-    })
+    });
    }, 300);
 
 
 }
 
 
+    function placeRandom() {
+
+        const available = [];
+
+         for (let i = 0; i < SIZE; i++) {
+             for (let j = 0; j < SIZE; j++) {
+                if (board[i][j] === 0){
+                    available.push({i, j})
+                }
+             }
+
+         }
 
 
+         if(available.length > 0) {
+            const randomCell = available[Math.floor(Math.random()* available.length)]
+            board[randomCell.i][randomCell.j] = Math.random() < 0.9? 2 : 4;
+
+            const cell = document.querySelector(`[data-row="${randomCell.i}"][data-col="${randomCell.j}"]`);
+            cell.classList.add('new-tile');
+    }
+
+}
+
+
+    function move(direction) {
+
+
+        let hasChanged = false;
+
+        if(direction === 'ArrowUp' || direction === 'ArrowDown' ) {
+         for (let j= 0; j < SIZE; j++) {
+            const column = [...Array(SIZE)].map((_, i) => board[i][j])
+
+            const newColumn = transform(column, direction === 'ArrowUp')
+
+            for (let i= 0; i < SIZE; i++) {
+                if( board[i][j] !== newColumn[i]) {
+                    hasChanged = true;
+                     board[i][j] = newColumn[i];
+                }
+
+            }
+
+         } 
+        }
+
+        else if (direction === 'ArrowLeft' || direction === 'ArrowRight') {
+            for (let i= 0; i < SIZE; i++) {
+            const row = board[i];
+            const newRow = transform(row, direction=== 'ArrowLeft');
+            
+            if (row.join(',') !==newRow.join(',')) {
+              hasChanged = true;
+               board[i] = newRow; 
+            }
+         }
+    }
+
+    if (hasChanged) {
+        placeRandom();
+        renderBoard();
+        checkGameOver();
+     }
+    }
+
+    function transform (line, moveTowardsStart) {
+        let newLine = line.filter(cell => cell !== 0 );
+
+        if (!moveTowardsStart) {
+            newLine.reverse();
+        }
+
+         for (let i= 0; i < newLine.length -1; i++) {
+            if(newLine[i] === newLine[i + 1]) {
+                newLine[i] *= 2;
+
+                updateScore(newLine[i]);
+
+                newLine.splice(i + 1, 1)
+            }
+         }
+
+         while (newLine.length < SIZE) {
+            newLine.push(0)
+         }
+
+          if (!moveTowardsStart) {
+            newLine.reverse();
+        }
+
+        return newLine;
+    }
+
+
+    function checkGameOver() {
+    for (let i= 0; i < SIZE; i++) {
+    for (let j= 0; j < SIZE; j++) {
+
+      if (board[i][j] === 0) {
+        return ;
+      }
+      
+      if (j < SIZE - 1 && board[i][j] === board[i][j + 1]) {
+        return;
+      }
+      
+      if (i < SIZE - 1 && board[i][j] === board[i + 1][j]) {
+        return;
+      }
+
+    }
+    }
+
+    gameOverElem.style.display = 'flex';
+
+    }
+
+    document.addEventListener('keydown' , event => {
+        if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+            move(event.key);
+        }
+    })
+
+    document.querySelector('.restart-game').addEventListener('click', restartGame);
+
+    initializeGame();
 
 
 
